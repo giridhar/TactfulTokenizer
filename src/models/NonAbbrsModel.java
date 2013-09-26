431f5a815b8b8d54adb68a5ad2e9dfff73e254f4
sujith <sujithonline8@gmail.com>
2013-09-26 19:27:31 +0530

    related to previous checkin

--- src/models/NonAbbrsModel.java
+++ src/models/NonAbbrsModel.java
@@ -4,28 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NonAbbrsModel {
 	
-	public static int getProbability(String nonAbbrWord) throws IOException{
+	public static Map<String, Integer> initialize() throws IOException{
 		int prob = 0;
 		String data = "";
+		Map<String, Integer> non_abbrs = new HashMap<String, Integer>();
 		InputStream inputStream  =  NonAbbrsModel.class.getResourceAsStream("/data/non_abbrs.txt");
 		BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-		data = br.readLine();
-		if(data != null){
-			String[] nawords = data.split(",");
-			for (int i = 0; i < nawords.length; i++)
-			{
-			  String[] wordArray = nawords[i].split("\\|\\|");
-				if(nonAbbrWord.equals(wordArray[0]))
+		while((data=br.readLine()) != null ){
+			if(data != null){
+				String[] nawords = data.split(",");
+				for (int i = 0; i < nawords.length; i++)
 				{
-					prob = 	Integer.parseInt(wordArray[1]);
-					return prob;
+				  String[] wordArray = nawords[i].split("\\|\\|");
+				  if (wordArray.length > 1)
+					non_abbrs.put(wordArray[0], Integer.parseInt(wordArray[1]));
+
 				}
 			}
-		}		
-	  return prob;				
+		}
+		br.close();
+	  return non_abbrs;				
 	}
-
 }
